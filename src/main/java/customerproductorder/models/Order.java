09ad3cc8c5af01461f6684268974a71a8821e6f0
Ozymandy <vault.schokk@gmail.com>
2016-07-11 11:59:17 +0300

    Task03 java convention fixed

--- src/main/java/customerproductorder/models/Order.java
+++ src/main/java/customerproductorder/models/Order.java
@@ -1,21 +1,49 @@
-
 package customerproductorder.models;
-import java.util.*;
 
+import java.util.List;
+
+/**
+ * Class of Order's model.
+ */
 public class Order {
+
+    /**
+     * Field which contains customer.
+     */
     private Customer customer;
+    /**
+     * Class that contains products of this order.
+     */
     private List<Product> products;
-    public Order(Customer customer, List<Product> products){
-        //this.product = product;
+
+    /**
+     * Ctor.
+     *
+     * @param customer
+     * @param products
+     */
+    public Order(Customer customer, List<Product> products) {
         this.products = products;
         this.customer = customer;
     }
+
+    /**
+     * Our implementation of toString() method
+     *
+     * @return class view
+     */
     @Override
-    public String toString(){
-        return "Customer " + customer.getFirstName() + " "+ customer.
+    public String toString() {
+        return "Customer " + customer.getFirstName() + " " + customer.
                 getName() + " ordered " + products.size() + " items";
     }
-    public Customer getCustomer(){
+
+    /**
+     * Access method of field customer.
+     *
+     * @return customer
+     */
+    public Customer getCustomer() {
         return this.customer;
     }
 }
--- src/main/java/customerproductorder/models/comparators/ModelComparator.java
+++ src/main/java/customerproductorder/models/comparators/ModelComparator.java
@@ -1,13 +1,21 @@
-
-
 package customerproductorder.models.comparators;
-import java.util.*;
-import customerproductorder.models.*;
 
+import java.util.Comparator;
+import customerproductorder.models.NameGetable;
+
+/**
+ * Our comparator.
+ */
 public class ModelComparator implements Comparator<NameGetable> {
+
+    /**
+     * Overrided method Comparator.compare()
+     *
+     * @return int result of comparing
+     */
     @Override
     public int compare(NameGetable t, NameGetable t1) {
-       return t1.getName().compareTo(t.getName());
+        return t1.getName().compareTo(t.getName());
     }
 
 }
