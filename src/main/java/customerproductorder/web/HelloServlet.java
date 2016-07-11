23618ba829a04d1528b276c02604b2d534e39afe
Ozymandy <vault.schokk@gmail.com>
2016-07-11 14:55:04 +0300

    Task 04

--- src/main/java/customerproductorder/web/HelloServlet.java
+++ src/main/java/customerproductorder/web/HelloServlet.java
@@ -2,6 +2,7 @@
 
 package customerproductorder.web;
 
+import ch.qos.logback.classic.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,18 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.slf4j.LoggerFactory;
+
      /**
     * This class provide us using servlets.
     */
 public class HelloServlet extends HttpServlet {
          /** This method is called when we send get to server.*/
+    private static final org.slf4j.Logger LOG =  LoggerFactory.getLogger(HelloServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         out.print("<h1>Hello from App developed by Kostya</h1>");
+        LOG.info("HelloWorld!");
 
     }
 }
-    }
-}
+
